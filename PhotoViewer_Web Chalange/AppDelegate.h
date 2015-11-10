70c5b4f73007834c22e1a2b71c99022064a76475
xhardcorex <nikshebeda@gmail.com>
2015-11-10 11:36:39 +0200

    Initial Commit

---
+++ PhotoViewer_Web Chalange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoViewer_Web Chalange
+//
+//  Created by Nik on 10.11.15.
+//  Copyright © 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoViewer_Web Chalange/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotoViewer_Web Chalange
+//
+//  Created by Nik on 10.11.15.
+//  Copyright © 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
